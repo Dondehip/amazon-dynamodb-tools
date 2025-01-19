2fa64879dfcbfeb0e9fed8571794e4e2413bc5ff
doncliuser-at-278322016574 <dondehip@amazon.com>
2025-01-19 16:10:15 -0500

    Changed the build file to add gRPC, but more work is needed.

--- ddb-hedging/build.gradle.kts
+++ ddb-hedging/build.gradle.kts
@@ -1,53 +1,69 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     java
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "com.dynamodbdemo"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 repositories {
     mavenCentral()
 }
 
+extra["springGrpcVersion"] = "0.3.0"
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.grpc:grpc-services")
+    implementation("org.springframework.grpc:spring-grpc-spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.grpc:spring-grpc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-    // AWS SDK v2.x BOM (Bill of Materials)
     implementation(platform("software.amazon.awssdk:bom:2.30.2"))
-
-    // DynamoDB v2.x dependencies
     implementation("software.amazon.awssdk:dynamodb")
-    implementation("software.amazon.awssdk:dynamodb-enhanced") // For higher-level DynamoDB operations
-
+    implementation("software.amazon.awssdk:dynamodb-enhanced")
     implementation("software.amazon.awssdk:netty-nio-client")
 
-    // CloudWatch v2.x dependencies
-//    implementation("software.amazon.awssdk:cloudwatch")
-//    implementation("software.amazon.awssdk:cloudwatchmetrics")
-
-    // https://mvnrepository.com/artifact/org.projectlombok/lombok
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
-    dependencies {
-    testImplementation("org.apache.commons:commons-lang3:3.14.0")
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.grpc:spring-grpc-dependencies:${property("springGrpcVersion")}")
+    }
 }
 
-//Run this task with like
-//  ./gradlew bootTestRun -DnumberOfRecordsToCreate=1000 -DnumberOfRecordsPerFile=100 -DddbTableName=MyDynamoDBTable
-tasks.bootTestRun {
-    systemProperties = System.getProperties().map { it.key.toString() to it.value.toString() }.toMap()
-    mainClass.set("com.dynamodbdemo.GenerateLoadTestData")
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc") {
+                    option("jakarta_omit")
+                    option("@generated=omit")
+                }
+            }
+        }
+    }
 }
-
 
 tasks.withType<Test> {
     useJUnitPlatform()
